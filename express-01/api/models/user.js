bc7c396267d77ad460af33013c05b3940befc868
Ingrid Beatriz Silva <108553706+ingrid-beatriiz@users.noreply.github.com>
2026-04-27 23:00:15 +0000

    adiciona hash no model de user

--- express-01/api/models/user.js
+++ express-01/api/models/user.js
@@ -1,3 +1,5 @@
+import argon2 from "argon2";
+
 const getUserModel = (sequelize, { DataTypes }) => {
   const User = sequelize.define("user", {
     username: {
@@ -14,12 +16,45 @@ const getUserModel = (sequelize, { DataTypes }) => {
       allowNull: false,
       validate: {
         notEmpty: true,
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [6, 55],
       },
     },
   });
 
   User.associate = (models) => {
     User.hasMany(models.Message, { onDelete: "CASCADE" });
+
+    User.hasMany(models.RefreshToken, {
+      onDelete: "CASCADE",
+    });
+  };
+
+  User.beforeCreate(async (user) => {
+    if (user.password) {
+      user.password = await user.generatePasswordHash(user.password);
+    }
+  });
+
+  User.beforeUpdate(async (user) => {
+    if (user.changed("password")) {
+      user.password = await user.generatePasswordHash(user.password);
+    }
+  });
+
+  User.prototype.generatePasswordHash = async function (password) {
+    return await argon2.hash(password);
+  };
+
+  User.prototype.validatePassword = async function (password) {
+    return await argon2.verify(this.password, password);
   };
 
   User.findByLogin = async (login) => {
